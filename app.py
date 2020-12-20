43ecfb630ee04d11a6b39e1de1bd232cc7a89303
tung <tungxuan0111@gmail.com>
2020-12-20 15:17:36 +1100

    none

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ from img_classification import predict
 from PIL import Image
 import streamlit as st
 from numpy import array
-# from fastbook import *
-# from fastai.vision.widgets import *
 
-# from tensorflow import keras
 
-uploaded_file = st.file_uploader("Show your fruit (banana, orange, apple or peach only)", type="jpg")
+uploaded_file = st.file_uploader("Show your fruit: apple, avocado, banana, cherry, grapes, kiwi, mango, orange, peach, pear, pineapple, strawberry or watermelon", type="jpg")
+
 if uploaded_file is not None:
-#     image = Image.open(uploaded_file).resize((128,128))
     image = Image.open(uploaded_file)
     st.image(image, caption='Your pic', use_column_width=True)
     st.write("Classifying. . .")
