a4d29927b74d69f41f5d0337f64196699913ee9f
tung <tungxuan0111@gmail.com>
2020-12-20 12:16:28 +1100

    none

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from img_classification import predict
 from PIL import Image
 import streamlit as st
-import numpy as np
-from tensorflow import keras
+from numpy import array
+from fastbook import *
+from fastai.vision.widgets import *
+# from tensorflow import keras
 
 uploaded_file = st.file_uploader("Show your fruit (banana, orange, apple or peach only)", type="jpg")
 if uploaded_file is not None:
-    image = Image.open(uploaded_file).resize((128,128))
+#     image = Image.open(uploaded_file).resize((128,128))
+    image = Image.open(uploaded_file)
     st.image(image, caption='Your pic', use_column_width=True)
     st.write("Classifying. . .")
     label = predict(image)
--- img_classification.py
+++ img_classification.py
@@ -1,23 +1,31 @@
-from tensorflow import keras
-import numpy as np
+# from tensorflow import keras
+# from fastai.vision.widgets import *
+from fastbook import load_learner
+from numpy import array
 
 def predict(image):
     
-    label_map = {'apple': 0, 'banana': 1, 'orange': 2, 'peach': 3}
+#     label_map = {'apple': 0, 'banana': 1, 'orange': 2, 'peach': 3}
+    learn_inf = load_learner('fruits.pkl')
+    vocab = learn_inf.dls.vocab
+    dict(zip(vocab, range(len(vocab))))
     
-    learn_inf = keras.models.load_model('fruits_model_2')
+#     learn_inf = keras.models.load_model('fruits_model_2')
     
-    x = np.array(image) / 255.
-#     x = keras.applications.vgg16.preprocess_input(
-#         x, data_format=None
-#     )
-    x = np.stack([x])
+    
+#     x = np.array(image) / 255.
+# #     x = keras.applications.vgg16.preprocess_input(
+# #         x, data_format=None
+# #     )
+#     x = np.stack([x])
 
-    predict = learn_inf.predict(x)     
-    idx = list(predict[0]).index(max(predict[0]))
+#     predict = learn_inf.predict(x)     
+    predict = learn_inf.predict(array(image))[0]
+#     idx = list(predict[0]).index(max(predict[0]))
     
-    for i in label_map.keys():
-        if label_map[i] == idx:
-            return str(i)
-    return 'some thing wrong'
+#     for i in label_map.keys():
+#         if label_map[i] == idx:
+#             return str(i)
+#     return 'some thing wrong'
+    return predict
    
