af49a043f8cf46963a18b266d401c06f8643bf69
tung <tungxuan0111@gmail.com>
2020-12-20 13:44:00 +1100

    none

--- img_classification.py
+++ img_classification.py
@@ -7,7 +7,7 @@ from numpy import array
 def predict(image):
     
 #     label_map = {'apple': 0, 'banana': 1, 'orange': 2, 'peach': 3}
-    learn_inf = load_learner('fruits.pkl')
+    learn_inf = load_learner(path='.')
     vocab = learn_inf.dls.vocab
     dict(zip(vocab, range(len(vocab))))
     
